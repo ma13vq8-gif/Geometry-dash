d8bae43ded12d5c164382966f44196be3d341cff
ma13vq8-gif <ma13vq8@gmail.com>
2026-04-23 12:36:57 +0200

    Add configuration settings and level data

--- Config.js
+++ Config.js
@@ -1,1 +1,16 @@
-
+{
+    "settings": {
+        "gravity": 0.8,
+        "jumpForce": -14,
+        "gameSpeed": 7,
+        "playerSize": 40,
+        "playerColor": "#00FF7F"
+    },
+    "levelData": [
+        {"type": "spike", "x": 600},
+        {"type": "spike", "x": 900},
+        {"type": "spike", "x": 1200},
+        {"type": "double_spike", "x": 1600},
+        {"type": "spike", "x": 2000}
+    ]
+}
